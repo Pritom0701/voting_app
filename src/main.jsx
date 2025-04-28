a87bebd00f0b14b410011e6abbe6db6f6bb1cdd1
ruddro42001~ <aliruddro@gmail.com>
2025-04-28 22:37:03 +0600

    'working'

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Home from './Pages/Home.jsx'
 import Result from './Pages/Result.jsx'
 import Registration_voter from './Pages/Registration_voter.jsx'
 import Registration_candidate from './Pages/Registration_candidate.jsx'
+import { Toaster } from 'react-hot-toast';
 // import Layout from './Layout/Layout.jsx';
 
 const router = createBrowserRouter([
@@ -40,5 +41,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Toaster />
   </StrictMode>,
 )
